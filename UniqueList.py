fe27727ad5c65398c4e01bc7b11cd51d6cb21949
Lucas Scalzo <l.scalzo90@gmail.com>
2020-12-12 13:08:22 -0600

    Add files via upload

---
+++ UniqueList.py
@@ -0,0 +1,13 @@
+
+
+#myUniqueList = ["empty list"]
+
+global myUniqueList
+myUniqueList = ["strings","integers","booleans","datatypes","functions","python"]
+
+
+myUniqueList.append(89)
+
+
+print(myUniqueList)
+
